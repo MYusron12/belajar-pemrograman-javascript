9989d13bf3bcb3ef59060b69614bd54caee19825
yusron1993 <ryusron19@gmail.com>
2019-11-04 20:21:40 +0700

    menambahkan file javascript while.js, angkot1.js, mengubah nama file html popupbox menjadi index, menambahkan file pengulangan

---
+++ latihan_javascript/while.js
@@ -0,0 +1,22 @@
+//invinite loop atau looping forever
+// while(true) {
+//     console.log('hello world');
+// }
+
+// var ulang = true;
+// while (ulang) {
+//     console.log('hello world!');
+//     ulang = confirm('lagi?');
+// }
+
+// var nilaiAwal = 1;
+// while (nilaiAwal <= 5) {
+//     console.log('hello world');
+//     nilaiAwal++;
+// }
+
+var nilaiAwal = 1;
+while (nilaiAwal <= 5) {
+    console.log('hello world' + nilaiAwal + 'x');
+    nilaiAwal++;
+}
