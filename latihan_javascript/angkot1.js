f182fece5a0e2716236329dd672603f86e12c478
yusron1993 <ryusron19@gmail.com>
2019-11-05 20:11:05 +0700

    menambahkan file baru angkot2, fro, pengulangan table, pengulangan for

--- latihan_javascript/angkot1.js
+++ latihan_javascript/angkot1.js
@@ -1,5 +1,6 @@
-var angkot = 1;
-while (angkot <= 10) {
-    console.log('Angkot No.' + angkot + ' beroperasi dengan baik');
-    angkot++;
+var jmlAngkot = 10;
+var noAngkot = 1;
+while (noAngkot <= jmlAngkot) {
+    console.log('Angkot No.' + noAngkot + ' beroperasi dengan baik');
+    noAngkot++;
 }
