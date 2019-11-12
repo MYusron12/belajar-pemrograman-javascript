a1ebce4d293b61d1662d71c178a017bbdb073a73
yusron1993 <ryusron19@gmail.com>
2019-11-12 19:33:27 +0700

    menambahkan file belajar baru

---
+++ latihan_javascript/scriptFunction.js
@@ -0,0 +1,14 @@
+//menghitung luas kubus
+function jumlahVolumeDuaKubus(a, b) {
+    var total, volumeA, volumeB;
+
+    volumeA = a * a * a;
+    volumeB = b * b * b;
+    total = volumeA + volumeB;
+
+
+    return total;
+}
+
+console.log(jumlahVolumeDuaKubus(8, 3));
+console.log(jumlahVolumeDuaKubus(5, 2));
