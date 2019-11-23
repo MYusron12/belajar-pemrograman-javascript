77664c07619ff9310e9ee648ebbc0441fbdee8d1
yusron1993 <ryusron19@gmail.com>
2019-11-23 20:08:39 +0700

    materi tentan array

---
+++ latihan_javascript/manipulasiArray.js
@@ -0,0 +1,44 @@
+//manipulasi
+
+//1. menambah isi array
+// var arr = [];
+// arr[0] = "muhammad";
+// arr[1] = "yusron";
+// arr[2] = "oke";
+
+// console.log(arr);
+
+//2. menghapus isi array
+// var arr = ["muhammad", "yusron", "ok"];
+// arr[1] = undefined;
+// console.log(arr);
+
+//3.menampilkan isis array
+// var arr = ["muhammad", "yusron", "ok"];
+
+// for(var y = 0; y < arr.length; y++) {
+//     console.log('orang nya' + y + '->' +
+//     arr[y]);
+// }
+
+
+
+
+
+//method array
+var arr = ["yusron","oke","waw"];
+//1. join
+// console.log(arr.join(' - '));
+
+//2. push & pop
+// arr.push("ganteng");
+// arr.pop()
+// console.log(arr.join(' - '));
+
+//3. unshift & shift
+// arr.unshift("mantap");
+// console.log(arr.join(' - '));
+arr.shift();
+console.log(arr.join(' - '));
+
+
