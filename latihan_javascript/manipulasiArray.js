29ecd13d410120e9beb8ec3564c8bbc2156d8371
yusron1993 <ryusron19@gmail.com>
2019-11-24 15:25:19 +0700

    pemahaman array pada javascript, membuat perbaikan applikasi juragan angkot

--- latihan_javascript/manipulasiArray.js
+++ latihan_javascript/manipulasiArray.js
@@ -26,7 +26,6 @@
 
 
 //method array
-var arr = ["yusron","oke","waw"];
 //1. join
 // console.log(arr.join(' - '));
 
@@ -38,7 +37,55 @@ var arr = ["yusron","oke","waw"];
 //3. unshift & shift
 // arr.unshift("mantap");
 // console.log(arr.join(' - '));
-arr.shift();
-console.log(arr.join(' - '));
+// arr.shift();
+// console.log(arr.join(' - '));
+
+//4. splice
+//splice(indexAwal, mauDihapusBerapa, elemenBaru1)
+// arr.splice(1, 2, 'mamah', 'aku');
+// console.log(arr.join(' - '));
+
+//5. slice
+//slice(awal, akhir);
+// var arr = ['yusron','oke','waw','mantap'];
+// var arr2 = arr.slice(2,3);
+// console.log(arr2.join(' - '));
+
+//6. forEach
+// var nomer = [1,2,3,4,5,6,7,8];
+// var nama = ['yusron', 'ok'];
+// for( var ok = 0; ok < nomer.length; ok++) {
+//     console.log(nomer[ok]);
+// }
+// nomer.forEach(function(e) {
+//     console.log(e);
+// });
+// nama.forEach(function(e, i) {
+//     console.log('mahasiswa ke-' + (i+1) + ' adalah : ' + e);
+// })
+
+
+//7. map
+// var angka = [2,3,4,2,2];
+// var angka2 = angka.map(function(e) {
+//     return e * 2;
+// });
+// console.log(angka2.join(' - '));
+
+//8. sort
+// var angka = [1,2,3,4,5,];
+// angka.sort(function(a,b) {
+//     return a-b;
+// });
+// console.log(angka.join(' - '));
+
+9. filter & find
+var angka = [2,3,5,45,46,67];
+var angka2 = angka.find(function(x) {
+    return x > 5;
+});
+console.log(angka2);
+
+
 
 
---
+++ latihan_javascript/pengelolaanAngkot.js
@@ -0,0 +1,69 @@
+var penumpang = [];
+var tambahPenumpang = function(namaPenumpang, penumpang) {
+    // jika angkot kosong
+    if( penumpang.length == 0) {
+        // tambah penumpang di awal array
+        penumpang.push(namaPenumpang);
+        // kembalikan isi array dan keluar dari function
+        return penumpang;
+    }
+    // else
+    else {
+        // telusuri seluruh kursi dari awal
+        for (var i = 0; i < penumpang.length; i++) {
+            // jika ada kursi kosong
+            if ( penumpang[i] == undefined ) {
+                // tambah penumpang di kursi tersebut
+                penumpang[i] = namaPenumpang;
+                // kembalikan isi array dan kelua dari function
+                return penumpang;
+            }
+            // jika sudah ada nama yang sama
+            else if( penumpang[i] == namaPenumpang ) {
+                // tampilkan pesan keslahannya
+                console.log(namaPenumpang + 'sudah ada dalam angkot');
+                // kembalikan isi array dan keluar dari function
+                return penumpang;
+            }
+            // jika seluruh kursi terisi
+            else if( i == penumpang.length - 1 ) {
+                // tambah penumpang di akhir array
+                penumpang.push(namaPenumpang);
+                // kembalikan isi arraydan keluar dari function
+                return penumpang;
+            }
+        }
+    }
+}
+
+
+var hapusPenumpang = function(namaPenumpang, penumpang) {
+    // jika angkot kosong
+    if( penumpang.length == 0 ) {
+        // tampilkan pesan bahwa angkot kosong
+        console.log('angkot kosong');
+        // tidak mungkin ada penumpang turun
+        // kembalikan isi array dan kelluar dari function
+        return penumpang;
+    }
+    // else
+    else {
+        // telusuri seluruh kursi dari awal
+        for( var i = 0; i < penumpang.length; i++) {
+            // jika nama penumpang sesuai
+            if( penumpang[i] == namaPenumpang ) {
+                // hapus penumpang dengan mengubah namanya menjadi indefined
+                penumpang[i] = undefined;
+                // kembalikan isi array dan keluar dari function
+                return penumpang;
+                // jika tidak ada nama yang sesuai
+            } else if( i == penumpang.length -1 ) {
+                // tampilkan pesan kesalahannya
+                console.log(namaPenumpang + 'nama tidak ada');
+                // kembalikan isi array dan keluar dari function
+                return penumpang;
+            }
+        }
+    }
+
+}
